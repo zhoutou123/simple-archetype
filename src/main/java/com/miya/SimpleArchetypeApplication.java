2267bd5613985cdb2eabbd940daf6de4ce4c2530
周拓 <zhoutuo@mi-ya.com.cn>
2018-07-03 10:55:03 +0800

    first commit

---
+++ src/main/java/com/miya/SimpleArchetypeApplication.java
@@ -0,0 +1,12 @@
+package com.miya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleArchetypeApplication.class, args);
+	}
+}
